17cd80f28d59d5427b849cbed940b6bf3d447e11
lizp <lizhiping0913@163.com>
2019-03-19 16:13:15 +0800

    简单工厂

--- src/main/java/com/gupao/lzp/pattern/factory/simplefactory/SimpleFactoryTest.java
+++ src/main/java/com/gupao/lzp/pattern/factory/simplefactory/SimpleFactoryTest.java
@@ -3,7 +3,7 @@ package com.gupao.lzp.pattern.factory.simplefactory;
 public class SimpleFactoryTest {
     public static void main(String[] args) {
 
-        //字符类型type
+        //字符 类型type
 //        Simplefactory simplefactory= new Simplefactory();
 //        simplefactory.create("诺基亚");
 
