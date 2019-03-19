449ba1045a1bb48b62317c894440db165dcedb70
lizp <lizhiping0913@163.com>
2019-03-19 15:59:38 +0800

    测试

--- src/main/java/com/gupao/lzp/pattern/factory/simplefactory/SimpleFactoryTest.java
+++ src/main/java/com/gupao/lzp/pattern/factory/simplefactory/SimpleFactoryTest.java
@@ -3,8 +3,10 @@ package com.gupao.lzp.pattern.factory.simplefactory;
 public class SimpleFactoryTest {
     public static void main(String[] args) {
 
+        //字符类型
 //        Simplefactory simplefactory= new Simplefactory();
 //        simplefactory.create("诺基亚");
+
 
 //         Simplefactory simplefactory =new Simplefactory();
 //         simplefactory.create("com.gupao.lzp.pattern.factory.simplefactory.XiaoMiMobile");
